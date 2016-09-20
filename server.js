68702aee64803a1f107c02a04332d7f83901f548
S.N.Jayaraman <snjay@hotmail.com>
2016-09-20 11:53:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article_one', function (req, res) {
-  res.send("Article One");
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article_two', function (req, res) {
   res.send("Article Two");
