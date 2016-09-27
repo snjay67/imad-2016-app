c0c5b3d00a04ee480c72b3048a74514105ba79cf
S.N.Jayaraman <snjay@hotmail.com>
2016-09-27 11:42:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,9 @@ console.log('Loaded!');
 var element = document.getElementById("titl");
 element.innerHTML = "New value";
 var img = document.getElementById("madi");
+console.log("Assigneg Img");
 img.onClick = function()
 {
+    console.log("Changing Margin");
     img.style.marginLeft = "100px";
 };
