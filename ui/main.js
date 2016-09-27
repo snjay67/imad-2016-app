2f39f681413c9419d43eb6ed8c786f779bec63f7
S.N.Jayaraman <snjay@hotmail.com>
2016-09-27 14:45:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,6 +8,6 @@ img.onClick = function (){    console.log("Changing Margin");    img.style.margi
 var img1 = document.getElementById("dvel");
 console.log("Assigned Img");
 console.log(img1);
-img1.onClick = function () {
+element.onClick = function () {
     console.log("Changing Margin");
     img.style.marginLeft = "100px";};
