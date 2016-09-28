ab565069d6a3dcf4f76cd76371cdfdb12bd9c6b5
S.N.Jayaraman <snjay@hotmail.com>
2016-09-28 21:31:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,9 +22,22 @@ console.log(img1);
 element.onclick = function () {
     console.log("Changing Margin");
     img1.style.marginLeft = "100px";};
+
 var button = document.getElementById("btn");
 button.onclick = function(){
     var counter = 0;
-    var count = document.getElementById("count");
-    count.innerHTML = counter.toString();
-}
+    var req = new XMLHttpRequest();
+    req.onreadystatechange = function(){
+        if (req.readystate == XMLRequest.DONE )
+        {
+            if (req.status == 200)
+            {
+                var counter = req.responseText;
+                var count = document.getElementById("count");
+                count.innerHTML = counter.toString();
+            }
+        }
+    };
+    request.open('GET','http://snjay67.imad.hasura-app.io/counter',true);
+    request.send();
+    };
