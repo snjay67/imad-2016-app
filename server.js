309df65e3802540d1fbd0aa58166b96b33f44950
S.N.Jayaraman <snjay@hotmail.com>
2016-10-22 20:16:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,7 +84,10 @@ const connectionString = 'postgres://localhost:5432/snjay67';
 
 const client = new pg.Client(connectionString);
 client.connect();
-    res.send(client);
+const query = client.query(
+  'CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
+query.on('end', () => { client.end(); }
+    res.send("Test");
     var articlename = req.params.article_name;
 //    res.send("test");
   res.send(get_template(articles[articlename]));
