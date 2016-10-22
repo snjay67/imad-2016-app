73652c55af614db7f5270173f6faf5787403d0e9
S.N.Jayaraman <snjay@hotmail.com>
2016-10-22 21:34:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ const query = client.query('SELECT * FROM user');
 query.on('row', (row) => {results.push(row); });
 query.on('end', () => {done(); return res.json(results);});
 
-    res.send(tstvar);
+    res.send(tstvar+connectionString);
     var articlename = req.params.article_name;
 //    res.send("test");
   res.send(get_template(articles[articlename]));
